fe7c6a93a8c75cf78bf3c1598937775585cf832f
Davyd Konopatskyi <40700475+DavidQ7@users.noreply.github.com>
2020-01-20 17:56:16 +0200

    add firebase config

--- Client/src/environments/environment.ts
+++ Client/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAtswxTvtZJVGSUGEreifO14rteQU-BzQE',
+    authDomain: 'hackaton20.firebaseapp.com',
+    databaseURL: 'https://hackaton20.firebaseio.com',
+    projectId: 'hackaton20',
+    storageBucket: 'hackaton20.appspot.com',
+    messagingSenderId: '232648889451',
+    appId: '1:232648889451:web:5fa872981e54b2f86e0d2e'
+  }
 };
 
 /*
