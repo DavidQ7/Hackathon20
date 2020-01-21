29ae352ca7a70ca5abb382b44d13d81dffc39dc4
Davyd Konopatskyi <40700475+DavidQ7@users.noreply.github.com>
2020-01-21 17:21:26 +0200

    ended user auth

--- Server/BLL/Service/UserService.cs
+++ Server/BLL/Service/UserService.cs
@@ -1,4 +1,6 @@
-﻿using Common.Models;
+﻿using AutoMapper;
+using Common.DbModels;
+using Common.Models;
 using DAL.Repositories;
 using System;
 using System.Collections.Generic;
@@ -9,18 +11,25 @@ namespace BLL.Service
 {
     public class UserService
     {
+        private readonly IMapper mapper;
         private readonly UserRepository userRepository;
 
-        public UserService(UserRepository userRepository)
+        public UserService(UserRepository userRepository, IMapper mapper)
         {
+            this.mapper = mapper;
             this.userRepository = userRepository;
         }
 
         public async Task<UserDTO> GetByEmail(string email)
         {
-           var user = await userRepository.GetByEmail(email);
-            //var bduser = mapper.Map<DTOUser>(user);
-            return null;
+            var user = await userRepository.GetByEmail(email);
+            var bduser = mapper.Map<UserDTO>(user);
+            return bduser;
+        }
+        public async Task Add(UserDTO user)
+        {
+            var dbuser = mapper.Map<User>(user);
+            await userRepository.Add(dbuser);
         }
     }
 }
