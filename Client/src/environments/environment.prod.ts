115bcfb4a80bd2e50bc152016270434015059d6c
Davyd Konopatskyi <40700475+DavidQ7@users.noreply.github.com>
2020-01-25 23:23:49 +0200

    add prod settings

--- Client/src/environments/environment.prod.ts
+++ Client/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  API : 'https://hackaton20.azurewebsites.net',
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAtswxTvtZJVGSUGEreifO14rteQU-BzQE',
+    authDomain: 'hackaton20.firebaseapp.com',
+    databaseURL: 'https://hackaton20.firebaseio.com',
+    projectId: 'hackaton20',
+    storageBucket: 'hackaton20.appspot.com',
+    messagingSenderId: '232648889451',
+    appId: '1:232648889451:web:5fa872981e54b2f86e0d2e'
+  }
 };
